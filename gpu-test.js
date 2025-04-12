c19abaaaeef909e669070767ed03478d55243960
davisdiehl <davisdiehl@gmail.com>
2025-04-12 02:52:45 +0000

    initial setup

---
+++ gpu-test.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox']
+  });
+
+  const page = await browser.newPage();
+  await page.goto('https://webgpu.report'); // Simple public test page
+
+  await page.waitForSelector('body');
+  const html = await page.content();
+
+  console.log('Page loaded. Some content:');
+  console.log(html.substring(0, 300)); // Log the first 300 chars
+
+  await browser.close();
+})();
